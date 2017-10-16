41e2cce0a358925104b493d6aad8041e7cbd8854
zkjaisq <18771964684@163.com>
2017-10-16 11:03:29 +0800

    first

---
+++ demo.js
@@ -0,0 +1,15 @@
+var fs =require('fs')
+var dirName =process.argv[2]
+fs.exists('[2]',function(exists){
+    if(exists){ console.log('yes');}
+    else{process.chdir("/Users/Administrator")
+        fs.mkdirSync("./"+dirName)
+        process.chdir("./+dirName")
+        fs.mkdirSync('css')
+        fs.mkdirSync('js')
+        fs.writeFileSync("index.html", "<!DOCTYPE><title>Hello<title><h1>Hi<h1>")
+        fs.writeFileSync("css/style.css", "h1{color: red}")
+        fs.writeFileSync("js/main.js", "war string = 'Hello Word'  alert(sreing)")
+        process.exit(0)   
+        }
+});
